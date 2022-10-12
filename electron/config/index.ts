7011ce0ee90ce7ddf5e23879fced1e0ec80c377a
hxl <hjk.sun@foxmail.com>
2022-10-12 15:34:07 +0800

    '第一次'

---
+++ electron/config/index.ts
@@ -0,0 +1,3 @@
+export const openIMApiAddress = "http://23.224.75.210:10002"
+export const openIMWsAddress = "ws://23.224.75.210:10001"
+export const sdkWsPort = "7788"
---
+++ src/constants/faceType.ts
@@ -0,0 +1,221 @@
+import face_108 from "@/assets/face/108.gif";
+import face_109 from "@/assets/face/109.gif";
+import face_110 from "@/assets/face/110.gif";
+import face_111 from "@/assets/face/111.gif";
+import face_112 from "@/assets/face/112.gif";
+import face_113 from "@/assets/face/113.gif";
+import face_114 from "@/assets/face/114.gif";
+import face_115 from "@/assets/face/115.gif";
+import face_116 from "@/assets/face/116.gif";
+import face_117 from "@/assets/face/117.gif";
+import face_118 from "@/assets/face/118.gif";
+import face_119 from "@/assets/face/119.gif";
+import face_120 from "@/assets/face/120.gif";
+import face_121 from "@/assets/face/121.gif";
+import face_122 from "@/assets/face/122.gif";
+import face_123 from "@/assets/face/123.gif";
+import face_124 from "@/assets/face/124.gif";
+import face_125 from "@/assets/face/125.gif";
+import face_126 from "@/assets/face/126.gif";
+import face_127 from "@/assets/face/127.gif";
+import face_128 from "@/assets/face/128.gif";
+import face_129 from "@/assets/face/129.gif";
+import face_130 from "@/assets/face/130.gif";
+import face_131 from "@/assets/face/131.gif";
+import face_132 from "@/assets/face/132.gif";
+import face_133 from "@/assets/face/133.gif";
+import face_134 from "@/assets/face/134.gif";
+import face_135 from "@/assets/face/135.gif";
+import face_136 from "@/assets/face/136.gif";
+import face_137 from "@/assets/face/137.gif";
+import face_138 from "@/assets/face/138.gif";
+import face_139 from "@/assets/face/139.gif";
+import face_140 from "@/assets/face/140.gif";
+import face_141 from "@/assets/face/141.gif";
+import face_142 from "@/assets/face/142.gif";
+import face_143 from "@/assets/face/143.gif";
+import face_144 from "@/assets/face/144.gif";
+import face_145 from "@/assets/face/145.gif";
+import face_146 from "@/assets/face/146.gif";
+import face_147 from "@/assets/face/147.gif";
+import face_148 from "@/assets/face/148.gif";
+import face_149 from "@/assets/face/149.gif";
+import face_150 from "@/assets/face/150.gif";
+import face_151 from "@/assets/face/151.gif";
+import face_152 from "@/assets/face/152.gif";
+import face_153 from "@/assets/face/153.gif";
+import face_154 from "@/assets/face/154.gif";
+import face_155 from "@/assets/face/155.gif";
+import face_156 from "@/assets/face/156.gif";
+import face_157 from "@/assets/face/157.gif";
+import face_158 from "@/assets/face/158.gif";
+import face_159 from "@/assets/face/159.gif";
+import face_160 from "@/assets/face/160.gif";
+import face_161 from "@/assets/face/161.gif";
+import face_162 from "@/assets/face/162.gif";
+import face_163 from "@/assets/face/163.gif";
+import face_164 from "@/assets/face/164.gif";
+import face_165 from "@/assets/face/165.gif";
+import face_166 from "@/assets/face/166.gif";
+import face_167 from "@/assets/face/167.gif";
+import face_168 from "@/assets/face/168.gif";
+import face_169 from "@/assets/face/169.gif";
+import face_170 from "@/assets/face/170.gif";
+import face_171 from "@/assets/face/171.gif";
+import face_172 from "@/assets/face/172.gif";
+import face_173 from "@/assets/face/173.gif";
+import face_174 from "@/assets/face/174.gif";
+import face_175 from "@/assets/face/175.gif";
+import face_176 from "@/assets/face/176.gif";
+import face_177 from "@/assets/face/177.gif";
+import face_178 from "@/assets/face/178.gif";
+import face_179 from "@/assets/face/179.gif";
+import face_180 from "@/assets/face/180.gif";
+import face_181 from "@/assets/face/181.gif";
+import face_182 from "@/assets/face/182.gif";
+import face_183 from "@/assets/face/183.gif";
+import face_184 from "@/assets/face/184.gif";
+import face_185 from "@/assets/face/185.gif";
+import face_186 from "@/assets/face/186.gif";
+import face_187 from "@/assets/face/187.gif";
+import face_188 from "@/assets/face/188.gif";
+import face_189 from "@/assets/face/189.gif";
+import face_190 from "@/assets/face/190.gif";
+import face_191 from "@/assets/face/191.gif";
+import face_192 from "@/assets/face/192.gif";
+import face_193 from "@/assets/face/193.gif";
+import face_194 from "@/assets/face/194.gif";
+import face_195 from "@/assets/face/195.gif";
+import face_196 from "@/assets/face/196.gif";
+import face_197 from "@/assets/face/197.gif";
+import face_198 from "@/assets/face/198.gif";
+import face_199 from "@/assets/face/199.gif";
+import face_200 from "@/assets/face/200.gif";
+import face_201 from "@/assets/face/201.gif";
+import face_202 from "@/assets/face/202.gif";
+import face_203 from "@/assets/face/203.gif";
+import face_204 from "@/assets/face/204.gif";
+import face_205 from "@/assets/face/205.gif";
+import face_206 from "@/assets/face/206.gif";
+import face_207 from "@/assets/face/207.gif";
+import face_208 from "@/assets/face/208.gif";
+import face_209 from "@/assets/face/209.gif";
+import face_210 from "@/assets/face/210.gif";
+import face_211 from "@/assets/face/211.gif";
+import face_212 from "@/assets/face/212.gif";
+import face_213 from "@/assets/face/213.gif";
+import face_214 from "@/assets/face/214.gif";
+import face_215 from "@/assets/face/215.gif";
+
+
+export const faceMap =
+  [
+    { context: "[发怒]", src: face_108 },
+    { context: "[旺柴]", src: face_109 },
+    { context: "[哇]", src: face_110 },
+    { context: "[合十]", src: face_111 },
+    { context: "[吃瓜]", src: face_112 },
+    { context: "[恐惧]", src: face_113 },
+    { context: "[汗]", src: face_114 },
+    { context: "[社会社会]", src: face_115 },
+    { context: "[抱拳]", src: face_116 },
+    { context: "[再见]", src: face_117 },
+    { context: "[囧]", src: face_118 },
+    { context: "[打脸]", src: face_119 },
+    { context: "[叹气]", src: face_120 },
+    { context: "[生病]", src: face_121 },
+    { context: "[天啊]", src: face_122 },
+    { context: "[庆祝]", src: face_123 },
+    { context: "[好的]", src: face_124 },
+    { context: "[破涕为笑]", src: face_125 },
+    { context: "[让我看看]", src: face_126 },
+    { context: "[失望]", src: face_127 },
+    { context: "[苦涩]", src: face_128 },
+    { context: "[笑脸]", src: face_129 },
+    { context: "[礼物]", src: face_130 },
+    { context: "[脸红]", src: face_131 },
+    { context: "[烟花]", src: face_132 },
+    { context: "[无语]", src: face_133 },
+    { context: "[Emm]", src: face_134 },
+    { context: "[裂开]", src: face_135 },
+    { context: "[翻白眼]", src: face_136 },
+    { context: "[加油]", src: face_137 },
+    { context: "[666]", src: face_138 },
+    { context: "[發]", src: face_139 },
+    { context: "[福]", src: face_140 },
+    { context: "[皱眉]", src: face_141 },
+    { context: "[耶]", src: face_142 },
+    { context: "[红包]", src: face_143 },
+    { context: "[机智]", src: face_144 },
+    { context: "[捂脸]", src: face_145 },
+    { context: "[奸笑]", src: face_146 },
+    { context: "[嘿哈]", src: face_147 },
+    { context: "[西瓜]", src: face_148 },
+    { context: "[鄙视]", src: face_149 },
+    { context: "[猪头]", src: face_150 },
+    { context: "[胜利]", src: face_151 },
+    { context: "[惊恐]", src: face_152 },
+    { context: "[OK]", src: face_153 },
+    { context: "[抠鼻]", src: face_154 },
+    { context: "[月亮]", src: face_155 },
+    { context: "[嘴唇]", src: face_156 },
+    { context: "[憨笑]", src: face_157 },
+    { context: "[亲亲]", src: face_158 },
+    { context: "[愉快]", src: face_159 },
+    { context: "[拥抱]", src: face_160 },
+    { context: "[爱心]", src: face_161 },
+    { context: "[敲打]", src: face_162 },
+    { context: "[呲牙]", src: face_163 },
+    { context: "[撇嘴]", src: face_164 },
+    { context: "[难过]", src: face_165 },
+    { context: "[拳头]", src: face_166 },
+    { context: "[凋谢]", src: face_167 },
+    { context: "[可怜]", src: face_168 },
+    { context: "[坏笑]", src: face_169 },
+    { context: "[调皮]", src: face_170 },
+    { context: "[衰]", src: face_171 },
+    { context: "[强]", src: face_172 },
+    { context: "[弱]", src: face_173 },
+    { context: "[快哭了]", src: face_174 },
+    { context: "[惊讶]", src: face_175 },
+    { context: "[太阳]", src: face_176 },
+    { context: "[擦汗]", src: face_177 },
+    { context: "[流泪]", src: face_178 },
+    { context: "[傲慢]", src: face_179 },
+    { context: "[微笑]", src: face_180 },
+    { context: "[阴险]", src: face_181 },
+    { context: "[白眼]", src: face_182 },
+    { context: "[睡]", src: face_183 },
+    { context: "[骷髅]", src: face_184 },
+    { context: "[闭嘴]", src: face_185 },
+    { context: "[害羞]", src: face_186 },
+    { context: "[委屈]", src: face_187 },
+    { context: "[疑问]", src: face_188 },
+    { context: "[嘘]", src: face_189 },
+    { context: "[握手]", src: face_190 },
+    { context: "[抓狂]", src: face_191 },
+    { context: "[发呆]", src: face_192 },
+    { context: "[咒骂]", src: face_193 },
+    { context: "[玫瑰]", src: face_194 },
+    { context: "[吐]", src: face_195 },
+    { context: "[便便]", src: face_196 },
+    { context: "[困]", src: face_197 },
+    { context: "[色]", src: face_198 },
+    { context: "[晕]", src: face_199 },
+    { context: "[奋斗]", src: face_200 },
+    { context: "[大哭]", src: face_201 },
+    { context: "[得意]", src: face_202 },
+    { context: "[悠闲]", src: face_203 },
+    { context: "[咖啡]", src: face_204 },
+    { context: "[菜刀]", src: face_205 },
+    { context: "[鼓掌]", src: face_206 },
+    { context: "[偷笑]", src: face_207 },
+    { context: "[蛋糕]", src: face_208 },
+    { context: "[心碎]", src: face_209 },
+    { context: "[炸弹]", src: face_210 },
+    { context: "[啤酒]", src: face_211 },
+    { context: "[勾引]", src: face_212 },
+    { context: "[右哼哼]", src: face_213 },
+    { context: "[尴尬]", src: face_214 },
+    { context: "[口罩]", src: face_215 },
+  ];
